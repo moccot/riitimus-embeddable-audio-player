54aafc41ff4abad0d9cc8575e0dfbd5d72f671bc
moccot <justother94@gmail.com>
2023-01-25 01:23:32 -0400

    Update riitimus

--- src/riitimus-embeddable-audio-player.js
+++ src/riitimus-embeddable-audio-player.js
@@ -10,6 +10,87 @@
  */
 
 /**
+ * RiitimusEmbeddableAudioPlayerElement class.
+ */
+class RiitimusEmbeddableAudioPlayerElement {
+    /**
+     * Creates a new RiitimusEmbeddableAudioPlayerElement instance.
+     * @param { string } htmlTag - The tag of the element to be created.
+     * @param { Object } attributes - The attributes to be set to the element.
+     */
+    constructor(htmlTag, attributes) {
+        /**
+         * The created html element.
+         * @type { HTMLElement }
+         */
+        this.htmlElement = document.createElement(htmlTag);
+
+        /**
+         * The child elements of the created element.
+         * @type { Object }
+         */
+        this.children = {};
+
+        /**
+         * A list containing all the event listeners set to the created element.
+         * @type { { event: string, callback: Function }[] }
+         */
+        this._eventListeners = [];
+
+        for (let attribute in attributes) {
+            this.htmlElement.setAttribute(attribute, attributes[attribute]);
+        }
+    }
+
+    /**
+     * Removes all added event listeners from the created element.
+     */
+    removeEventListeners() {
+        for (let eventListenerData of this._eventListeners) {
+            this.htmlElement.removeEventListener(eventListenerData.event, eventListenerData.callback);
+        }
+    }
+
+    /**
+     * Sets the created element's innerText property.
+     * @param { string } text - The new value for the created element's innerText property.
+     * @returns this, for chainability.
+     */
+    setText(text) {
+        this.htmlElement.innerText = text;
+
+        return this;
+    }
+
+    /**
+     * Adds a child element to the created element.
+     * @param { string } name - The name of the child element for indexing it in the children list.
+     * @param { RiitimusEmbeddableAudioPlayerElement } child - The child element to be added.
+     * @returns this, for chainability.
+     */
+    addChild(name, child) {
+        this.children[name] = child;
+        this.htmlElement.appendChild(child.htmlElement);
+
+        return this;
+    }
+
+    /**
+     * Adds an event listener to the created element.
+     * @param { string } event - The name of the event to be added.
+     * @param { Function } callback - The event's callback.
+     * @returns this, for chainability.
+     */
+    on(event, callback) {
+        this._eventListeners.push({ event, callback });
+
+        this.htmlElement.addEventListener(event, callback);
+
+        return this;
+    }
+}
+
+/**
  * RiitimusEmbeddableAudioPlayer class.
  */
 class RiitimusEmbeddableAudioPlayer {
@@ -24,10 +105,10 @@ class RiitimusEmbeddableAudioPlayer {
     constructor(srcs) {
         /**
          * The current audio that is being played.
-         * @type { HTMLAudioElement | null }
+         * @type { HTMLAudioElement }
          * @default
          */
-        this._currentAudio = null;
+        this._currentAudio = this._createElement({ tag: 'audio' });
 
         /**
          * The current audio's cover image.
@@ -48,6 +129,27 @@ class RiitimusEmbeddableAudioPlayer {
          * @type { RiitimusEmbeddableAudioPlayerSrcStructure[] }
          */
         this._srcs = srcs;
+
+        this._elements = {};
+
+        this._mount();
+    }
+
+    /**
+     * Creates a new RiitimusEmbeddableAudioPlayerElement instance.
+     * @param { { tag: string, attributes: Object, children: { name: string, tag: string, attributes: Object }[] } } options 
+     * @returns { RiitimusEmbeddableAudioPlayerElement }
+     */
+    _createElement(options) {
+        let element = new RiitimusEmbeddableAudioPlayerElement(options.tag, options.attributes);
+
+        if (options.children && options.children.length > 0) for (let childElementOption of options.children) {
+            let childElement = this._createElement(childElementOption);
+
+            element.addChild(childElementOption.name, childElement);
+        }
+
+        return element;
     }
 
     /**
@@ -89,5 +191,125 @@ class RiitimusEmbeddableAudioPlayer {
 
         if (currentAudioIsPaused) this._playAudio();
         else this._pauseAudio();
+    }
+
+    /**
+     * Mounts the player in the page
+     */
+    _mount() {
+        /**
+         * Creates a new RiitimusEmbeddableAudioPlayerElement instance representing an icon.
+         * @param { string } iconName - The name of the icon in Google Fonts Icons.
+         * @returns { RiitimusEmbeddableAudioPlayerElement }
+         */
+        const createIcon = iconName => {
+            return this._createElement({ tag: 'span', attributes: { class: 'material-symbols-rounded' } }).setText(iconName);
+        }
+
+        let container = document.getElementById('riitimus-embeddable-audio-player-container');
+
+        this._elements.audioCoverImageContainer = this._createElement({
+            tag: 'div',
+
+            attributes: {
+                class: 'audio-cover-image-container',
+            },
+
+            children: [
+                {
+                    name: 'audioCoverImage',
+                    tag: 'img',
+                    attributes: {
+                        class: 'audio-cover-image'
+                    }
+                }
+            ]
+        });
+
+        this._elements.audioTitle = this._createElement({
+            tag: 'span',
+            attributes: {
+                class: 'audio-title'
+            }
+        });
+
+        this._elements.audioVolumeControls = this._createElement({
+            tag: 'div',
+            attributes: {
+                class: 'audio-volume-controls'
+            },
+            children: [
+                {
+                    name: 'audioVolumeBar',
+                    tag: 'input',
+                    attributes: {
+                        class: 'audio-volume-bar',
+                        type: 'range'
+                    }
+                },
+                {
+                    name: 'audioVolumeMuteControl',
+                    tag: 'button',
+                    attributes: {
+                        class: 'audio-volume-mute-control'
+                    }
+                }
+            ]
+        });
+
+        this._elements.audioControls = this._createElement({
+            tag: 'div',
+            attributes: {
+                class: 'audio-controls'
+            },
+            children: [
+                {
+                    name: 'audioPlaybackProgressControl',
+                    tag: 'input',
+                    attributes: {
+                        class: 'audio-playback-progress-control',
+                        type: 'range'
+                    }
+                },
+                {
+                    name: 'audioSkipPreviousControl',
+                    tag: 'button',
+                    attributes: {
+                        class: 'audio-skip-previous-control'
+                    }
+                },
+                {
+                    name: 'audioTogglePlaybackControl',
+                    tag: 'button',
+                    attributes: {
+                        class: 'audio-toggle-playback-control'
+                    }
+                },
+                {
+                    name: 'audioSkipNextControl',
+                    tag: 'button',
+                    attributes: {
+                        class: 'audio-skip-next-control'
+                    }
+                }
+            ]
+        });
+
+        for (let index in this._elements) {
+            let element = this._elements[index];
+
+            if (index === 'audioVolumeControls') element.children.audioVolumeMuteControl.addChild('icon', createIcon('volume_up'));
+
+            else if (index === 'audioControls') {
+                element.children.audioSkipPreviousControl.addChild('icon', createIcon('skip_previous'));
+                
+                element.children.audioTogglePlaybackControl.addChild('playIcon', createIcon('play_arrow'))
+                element.children.audioTogglePlaybackControl.addChild('pauseIcon', createIcon('pause'));
+
+                element.children.audioSkipNextControl.addChild('icon', createIcon('skip_next'));
+            }
+
+            container.appendChild(element.htmlElement);
+        }
     }
 }
